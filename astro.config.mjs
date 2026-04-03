be7da1119fed028142498ca32e0bc3c035c8037d
Hanif Nurrahmat <nurrahmathaniff@gmail.com>
2026-04-04 00:24:51 +0700

    Create sitemap and thumbnail

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ziel.work',  
   vite: {
       plugins: [tailwindcss()],
     },
 
-  integrations: [react()],
+ integrations: [react(), sitemap()],
   adapter: netlify(),
 });
